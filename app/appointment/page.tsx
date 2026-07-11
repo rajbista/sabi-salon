225b7aafc18a440fc40afdd941fb6da133912d5d
Raj Bista <tbista@coca-cola.com>
2026-07-11 16:59:01 -0500

    Integrated square appointment booking service

--- app/appointment/page.tsx
+++ app/appointment/page.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 
-// export default function Appointment() {
-//   return <div>Appointment</div>;
-// }
-
 const BookingPage = () => {
   const bookingUrl =
     "https://book.squareup.com/appointments/i3cqi85ohchtbg/location/L42P9GCP7GMCB/services";
-
   return (
     <main className="booking-page">
-      {/* <section className="booking-header"> */}
-      {/* <h1>Book Your Appointment</h1> */}
-      {/* <p>Select your service, preferred date, and appointment time below.</p> */}
-      {/* </section> */}
-
       <div className="booking-container">
         <iframe
           src={bookingUrl}
